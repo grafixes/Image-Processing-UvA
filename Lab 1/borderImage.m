f20bf7cab14767edc0bdb7d1d748ca5de261f7b7
grafixes <maxbos@MacBook-Pro-van-Max.local>
2017-04-14 22:24:11 +0200

    borderImage now uses imageRotationMargins

--- Lab 1/borderImage.m
+++ Lab 1/borderImage.m
@@ -3,15 +3,8 @@
 % Positions the original image in the center of this new dimension.
 function borderedImage = borderImage(image, angle)
 
-    [height, width] = size(image);
-    angle = abs(angle);
-    newWidth = abs(height * cos(angle) + width * sin(angle));
-    newHeight = abs(height * sin(angle) + width * cos(angle));
-    
-    topMargin = ceil((newWidth - width) / 2);
-    leftMargin = ceil((newHeight - height) / 2);
-    newWidth = width + leftMargin*2;
-    newHeight = height + topMargin*2;
+    [newHeight, newWidth, topMargin, ...
+        leftMargin] = imageRotationMargins(image, angle);
     
     borderedImage = zeros(newHeight, newWidth);
     borderedImage(topMargin+1:(newHeight-topMargin), ...
