0f9c150a2fd2047bbb2f693575171f0b089a64b8
grafixes <maxbos@wcw-stud-145-109-19-4.wireless.uva.nl>
2017-05-17 15:35:48 +0200

    Added dataset division

---
+++ Lab 5/matlab/main.m
@@ -0,0 +1,16 @@
+%% Exercise 5
+% Max Bos (10669027) and Haischel Dabian (10336230)
+
+function main()
+
+    clear
+    close all
+    
+    %% (Sec.2) Principal Component Analysis
+    load 'omni.mat'
+    
+    train_set = [images{1:300}]
+    test_set = [images{301:550}]
+    
+    
+end
